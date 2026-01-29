9a897987531a582ec9e0c0193e558675d0fbba1c
Sree3104 <sreeman312004@gmail.com>
2026-01-29 19:41:50 +0000

    initial

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+
+  required_version = ">= 1.3.0"
+
+}
+
+
+
+provider "aws" {
+
+  region = "ap-south-1"
+
+}
+
+
+
+resource "random_id" "suffix" {
+
+  byte_length = 4
+
+}
+
+
+
+resource "aws_s3_bucket" "gitops_demo" {
+
+  bucket = "gitops-demo-${random_id.suffix.hex}"
+
+}
+
+
